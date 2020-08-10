1d67dac421a084894cb420033734d2437a0fc5fe
nathanshaw <nathanshawsemail@gmail.com>
2020-08-09 20:36:06 -0700

    think I have a first working version, need to test the code out

--- WeatherManager.h
+++ WeatherManager.h
@@ -2,34 +2,86 @@
 #define __WEATHER_MANAGER_H__
 
 // #include "../ValueTracker.h"
+#include "SHTSensor.h"
 
 class WeatherManager {
     public:
         WeatherManager(float h, float t);
+        bool setup();
         bool update();
+        // bool setAccuracy();
+
     private:
+        ////////////////// Sensor /////////////////////////
         float sensor_active = false;
+        SHTSensor & sensor;
+
         /////////////////// Humidity //////////////////////
         float humid = 0.0;
         float last_humid = 0.0;
         float humid_high_thresh;
+        // this will indicate if the WeatherManager recommends
+        // a shutdown due to high humidity
+        bool  humid_shutdown = false;
 
         /////////////////// Temperature ///////////////////
         float temp = 0.0;
         float last_temp = 0.0;
+        // indicates how much historesis is applied to the
+        // high temp threshold to return to normal operation
+        // this indicates if a temperature shutdown is recommended
+        float temp_historesis;
+        bool humid_shutdown = false;
 };
 
-WeatherManager::WeatherManager(float h, float t) {
+WeatherManager::WeatherManager(float h, float t, float h) {
     humid_high_thresh = h;
     temp_high_thresh = t;
+    temp_historesis = h;
+}
+
+WeatherManager::setup() {
+    if (sensor.init()) {
+        Serial.println("SHT temp/humid sensor was initialised");
+        sensor_active = true;
+        return true;
+
+    } else {
+        for (int i = 0) i < 1000; i++) {
+            Serial.print("ERROR, SHT init() failed, there will be no active temp/humid sensor");
+            delay(500);
+        }
+        sensor_active = false;
+        return false;
+    }
 }
 
 bool update(){
-    // see if it is time to take a new reading
-    // if it is then take a new reading
-    // update the current and last values
-    // TODO - also track the delta over time (is it heating up?)
-    // return false if no update is made and true if an update is made
+    // return false if no update is made and true 
+    // if an update is made
+    // check to see if the sensor is ready for a new reading
+    if (sensor.readSample()) {
+        last_humid = humid;
+        humid = sensor.getHumidity();
+        if (humid >= humid_high_thresh) {
+            humid_shutdown = true;
+            return true;
+        }
+        last_temp = temp;
+        temp = sensor.getTemperature();
+        if (temp >= temp_high_thresh && temp_shutdown == false) {
+            temp_shutdown = true;
+            return true;
+        } else if (temp <= (temp_high_thresh * (1.0 - temp_historesis))){ 
+            temp_shutdown = false;
+        }
+    } else {
+        Serial.println("SHT sensor is not ready for a new reading, exiting update");
+    }
+
+    // if we make it this far then there are no emergency shudown
+    // conditions and we can exit the program
+    return false;
 }
 
 
