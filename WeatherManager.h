ea77f28c5ae2b059927aa30d0cfbc7a378bec9bb
Nathan Villicana-Shaw <nathan@Nathans-MacBook-Pro.local>
2020-09-21 15:04:37 -0700

    moving on

--- WeatherManager.h
+++ WeatherManager.h
@@ -1,7 +1,7 @@
 #ifndef __WEATHER_MANAGER_H__
 #define __WEATHER_MANAGER_H__
 
-// #include "../ValueTracker.h"
+#include "../ValueTracker/ValueTrackerFloat.h"
 #include "SHTSensor.h"
 #include "Wire.h"
 
@@ -19,7 +19,8 @@ class WeatherManager {
 
         /////////////////// Humidity //////////////////////
         float humid = 0.0;
-        float last_humid = 0.0;
+        ValueTrackerFloat humid_tracker(*humid);
+
         float humid_high_thresh;
         // this will indicate if the WeatherManager recommends
         // a shutdown due to high humidity
@@ -27,6 +28,8 @@ class WeatherManager {
 
         /////////////////// Temperature ///////////////////
         float temp = 0.0;
+        ValueTrackerFloat temp_tracker(*temp);
+
         float last_temp = 0.0;
         float temp_high_thresh;
         // indicates how much historesis is applied to the
@@ -49,6 +52,7 @@ bool WeatherManager::init() {
     if (sensor.init()) {
         Serial.println("SHT temp/humid sensor was initialised");
         sensor_active = true;
+        // TODO need to set the sensor integration time and sensor gain
         return true;
     } else {
         for (int i = 0; i < 1000; i++) {
@@ -66,14 +70,14 @@ bool WeatherManager::update(){
     // if an update is made
     // check to see if the sensor is ready for a new reading
     if (sensor.readSample()) {
-        last_humid = humid;
         humid = sensor.getHumidity();
+        humid_tracker.update()
         if (humid >= humid_high_thresh) {
             humid_shutdown = true;
             return true;
         }
-        last_temp = temp;
         temp = sensor.getTemperature();
+        temp_tracker.update();
         if (temp >= temp_high_thresh && temp_shutdown == false) {
             temp_shutdown = true;
             return true;
@@ -83,7 +87,6 @@ bool WeatherManager::update(){
     } else {
         Serial.println("SHT sensor is not ready for a new reading, exiting update");
     }
-
     // if we make it this far then there are no emergency shudown
     // conditions and we can exit the program
     return false;
